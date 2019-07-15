2a8b0c302313ca550c3a8aa39bede468d2ec4d5b
chishaokaodezhu <write_bugs@163.com>
2019-07-15 14:22:35 +0800

    简要需求与内存映射技术的测试工程
    1. 简单说明大致需求
    2. 新增测试工程，用于测试相关技术的使用

---
+++ CurveTool/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        public void Save(int data)
+        {
+            MemoryMappedFile memory = MemoryMappedFile.CreateFromFile("test.data", System.IO.FileMode.OpenOrCreate, "img", 1024);
+            MemoryMappedViewAccessor accessor1 = memory.CreateViewAccessor();
+
+            accessor1.Write(0, data);  // 在指定位置写入int值
+            accessor1.Dispose();        //
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Save(0x32323232);
+        }
+    }
+}
