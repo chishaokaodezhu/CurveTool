48e3d143bd18f8310bd9d26ed49038c2c30f4938
chishaokaodezhu <write_bugs@163.com>
2019-07-17 22:30:23 +0800

    添加曲线绘制部分的简单架构代码

---
+++ CurveTool/CurveMonitor/src/Graph/CurveView.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * 本类继承自WPF提供的绘图单元，用于曲线图的显示。绘制时该类将使用DrawingVisual进行
+ * 折线图的绘制，绘制完成后才会将绘制好的Visual交付给WPF绘图单元
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurveMonitor.src.Graph
+{
+    public class CurveView
+    {
+
+    }
+}
---
+++ CurveTool/CurveMonitor/src/Graph/CurveWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿/*
+ *  此类对应着曲线显示窗口。该窗口包含了曲线绘制视图、交互控件等。该窗口可以
+ *  单独被创建，也可以新建一个线程，然后在新线程中创建。建议使用后一种方式，
+ *  因为当曲线绘制时如果数据点较多时绘制可能占用大量CPU资源。
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CurveMonitor.src.Graph
+{
+    /// <summary>
+    /// CurveWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CurveWindow : Window
+    {
+        public CurveWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * 外部通过此方法向窗口添加曲线数据
+         */
+        public void DeliverData(float[] data)
+        {
+
+        }
+
+        /*
+         * 外部通过此方法进行曲线的绘制动作。该方法会调用本窗口的Dispatcher来
+         * 处理绘制任务，让UI线程执行ReDraw()。如果本窗口是由非主UI线程创建的，
+         * 那么ReDraw()将会在此线程中被执行。
+         */
+        public void CurveUpdate()
+        {
+            
+        }
+
+        private void ReDraw()
+        {
+
+        }
+    }
+}
